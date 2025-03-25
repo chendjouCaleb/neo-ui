2d989f51d3ba51263a80d1da90396f7f2233ca42
chendjou2016 <chendjou2016@outlook.fr>
2025-03-25 16:30:24 +0100

    timetable color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,43 @@
 import {Component} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {TextFieldPage} from './textField/textField.page';
 import {DropdownPage} from './dropdown/dropdown.page';
 import {ContactPage} from './contact/contact.page';
 import {SelectPage} from './select/select.page';
+import {MySwitch} from '../NeoUI/switch';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,  TextFieldPage, DropdownPage, ContactPage, SelectPage],
+  imports: [RouterOutlet, TextFieldPage, DropdownPage, ContactPage, SelectPage, RouterLink, RouterLinkActive, MySwitch],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  host: {
+    '[class.fluent-light-theme]': "themeMode === 'light'",
+    '[class.fluent-dark-theme]': "themeMode === 'dark'",
+  }
 })
 export class AppComponent {
   title = 'NeoUI';
+  themeMode = this.getThemeMode();
+
+  toggleThemeMode(isDark: boolean) {
+    const theme = isDark ? 'dark' : 'light';
+    this.themeMode = theme
+    localStorage.setItem('theme', theme)
+  }
+
+  getThemeMode(): 'dark' | 'light' {
+    const darkModeItem = localStorage.getItem('theme');
+    if(!darkModeItem) {
+      return 'light'
+    }
+    if(darkModeItem.toLowerCase() === 'light') {
+      return 'light'
+    }
+    if(darkModeItem.toLowerCase() === 'dark') {
+      return 'dark'
+    }
+    return 'light'
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -17,23 +17,28 @@ import {PagerContainer} from '../NeoUI';
 import {PagerPage} from './pager/pager.page';
 import {MonthCalendarPage} from './agenda-month/month-calendar.page';
 import {ButtonPage} from './button/button.page';
+import {SelectPage} from './select/select.page';
 
 export const routes: Routes = [
   {path: '', component: ButtonPage },
+  {path: 'button', component: ButtonPage },
   {path: 'pager', component: PagerPage },
   {path: 'tooltip', component: TooltipPage },
   {path: 'switch', component: SwitchPage },
   {path: 'toast', component: ToastPage },
+  {path: 'select', component: SelectPage },
   {path: 'popover', component: PopoverPage },
-  {path: 'timetable', component: TimetablePage},
+
   {path: 'checkbox', component: CheckboxPage},
   {path: 'persona', component: PersonaPage},
   {path: 'dropdown', component: DropdownPage},
   {path: 'horizontal-pager', component: HorizontalPagerPage},
+
+  {path: 'timetable', component: TimetablePage},
   {path: 'month-calendar', component: MonthCalendarPage},
   {path: 'agenda', component: AgendaPage},
   {path: 'agenda-planning', component: AgendaPlanningPage},
   {path: 'agenda-month', component: AgendaMonthPage},
   {path: 'timetable-planning', component: TimetablePlanningPage},
-  {path: 'button', component: ButtonPage }
+
 ];
